9c43b5e6479a944157ca8a417378b8248dcab329
Cade Preister <cadepreister@unomaha.edu>
2024-07-09 22:05:12 -0500

    new responses

--- response.py
+++ response.py
@@ -7,5 +7,7 @@ def get_response(user_input):
         return '....nothing?'
     elif 'hi' in lowered:
         return 'Welcome to Montana State!'
+    elif 'what server is this?' in lowered:
+        return 'A test server of course!!'
     else:
         return choice(['I dont understand...', 'Try again', 'Can you say that any differently?'])
